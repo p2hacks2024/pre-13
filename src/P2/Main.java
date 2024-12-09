5197a40c1ffda2343a3a496b21b4b0feb8bbd5e6
S110-0567 <b1023042@fun.ac.jp>
2024-12-09 20:28:41 +0900

    再変更

--- src/P2/Main.java
+++ src/P2/Main.java
@@ -4,37 +4,19 @@ package P2;
 // カード情報
 
 public class Main{
-	
-	
 
 	public static void main(String[] args) {
-		CookingFlavor.startDrawElseCards(args);
-		CPU.startCpuDrawElseCards(args);
-    
-		//while()
-		Tohu.drawTohuCards(args);
-		CookingFlavor.drawElseCards(args);
-    System.out.println();
-    
-		CPU.cpuDrawTohuCards(args);
-		CPU.cpuDrawElseCards(args);
-		System.out.println();
 
-		TradeCards.TradeCards();
+		Tohu.drawTohuCards(args);
+		CookingFlavor.startDrawElseCards(args);
+		TradeCards.TradeCard();
 		DecideCards.convertNumber();
 		DecideCards.calculateTohu();
 		DecideCards.calculateFlavor();
 		DecideRoles.decideRoleNumber();
-<<<<<<< HEAD
 		DecideDishes.selectDishes();
 		SelectFlavor.canCreateDish();
 		//dishNumberで色々処理できるはず
-=======
-    
-		SelectFlavor.SelectUsingFlavor();
->>>>>>> f858078e35168de04ad5b47ac371e9ecc1befece
 
-    CPU.cpuTradeCards();
-		//CPU.cpuSelectFlavor
 	}
 }
--- src/P2/TradeCards.java
+++ src/P2/TradeCards.java
@@ -16,14 +16,14 @@ public class TradeCards extends Tohu{
 		System.out.println("カードを交換しますか？ yes/no");
 		Scanner tradeScan = new Scanner(System.in);
 		String trade = tradeScan.next();
-		
+
 		while(moreCard == false){
 			if(trade.equals("no")) {
 				break;
 			}else {				
 
 				//カードを指定して交換ボタンを押す(今回は仮で入力する)
-				
+
 				System.out.println("1~5で変えたいカードの番号をどうぞ");
 				Scanner scan = new Scanner(System.in);
 				int tradeNumber = scan.nextInt();
@@ -33,7 +33,6 @@ public class TradeCards extends Tohu{
 				myCards[tradeNumber-1] = cards[r.nextInt(3)];
 				//ここで満腹度を増やす
 
-<<<<<<< HEAD
 				for (int i = 0; i < myCards.length; i++) {
 					System.out.println(myCards[i]);
 				}
@@ -49,28 +48,8 @@ public class TradeCards extends Tohu{
 				if(more.equals("no")) {
 					break;
 				}
-				
-=======
-			for (int i = 0; i < myCards.length; i++) {
-				System.out.println(myCards[i]);
-			}
 
-			for (int i = 0; i < numberOfCards; i++) {
-				System.out.println(myFlavors[i]);
-			}
 
-			//この時点で満腹度が上限-1なら交換できないようにする
-      System.out.println();
-			System.out.println("まだ交換しますか？ yes/no");
-
-			String more = scan.next();
-
-			if(!more.equals("no")) {
-				TradeCard();
-			}else {
-				moreCard = true;
-				System.out.println();
->>>>>>> f858078e35168de04ad5b47ac371e9ecc1befece
 			}
 		}
 	}
