4c730db05ab11a156d3bef43365db2f555cef639
S110-0567 <b1023042@fun.ac.jp>
2024-12-10 21:14:09 +0900

    基本形できました

--- src/P2/DecideDishes.java
+++ src/P2/DecideDishes.java
@@ -63,8 +63,7 @@ public class DecideDishes extends DecideRoles{
 
 			if(dishNumber == 0) { //冷奴
 				if(FlavorX % 11 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "醤油") {
@@ -80,8 +79,7 @@ public class DecideDishes extends DecideRoles{
 			}
 			if(dishNumber == 1) { //豆腐の味噌漬け
 				if(FlavorX % 13 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "味噌") {
@@ -97,8 +95,7 @@ public class DecideDishes extends DecideRoles{
 			}
 			if(dishNumber == 2) { //豆腐ハンバーグ
 				if(FlavorX % 23 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "肉") {
@@ -114,8 +111,7 @@ public class DecideDishes extends DecideRoles{
 			}
 			if(dishNumber == 3) { //焼き豆腐
 				if(FlavorX % 29 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "ネギ") {
@@ -134,8 +130,7 @@ public class DecideDishes extends DecideRoles{
 
 			if(dishNumber == 4) { //ネギ乗せ冷奴
 				if(FlavorX % 319 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "醤油") {
@@ -149,12 +144,13 @@ public class DecideDishes extends DecideRoles{
 					}
 					System.out.println("ネギ乗せ冷奴を作って勝負します");
 					nextGauge = 3;
+					canCreate = true;
+					break;
 				}
 			}
 			if(dishNumber == 5) { //麻婆豆腐
 				if(FlavorX % 391 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "辛味調味料") {
@@ -168,12 +164,13 @@ public class DecideDishes extends DecideRoles{
 					}
 					System.out.println("麻婆豆腐を作って勝負します");
 					nextGauge = 3;
+					canCreate = true;
+					break;
 				}
 			}
 			if(dishNumber == 6) { //豆腐の煮物
 				if(FlavorX % 407 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "醤油") {
@@ -187,12 +184,13 @@ public class DecideDishes extends DecideRoles{
 					}
 					System.out.println("豆腐の煮物を作って勝負します");
 					nextGauge = 3;
+					canCreate = true;
+					break;
 				}
 			}
 			if(dishNumber == 7) { //辛味豆腐サラダ
 				if(FlavorX % 527 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "辛味調味料") {
@@ -206,12 +204,13 @@ public class DecideDishes extends DecideRoles{
 					}
 					System.out.println("辛味豆腐サラダを作って勝負します");
 					nextGauge = 3;
+					canCreate = true;
+					break;
 				}
 			}
 			if(dishNumber == 8) { //出汁ジュレ冷奴
 				if(FlavorX % 551 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "出汁") {
@@ -225,6 +224,8 @@ public class DecideDishes extends DecideRoles{
 					}
 					System.out.println("出汁ジュレ冷奴を作って勝負します");
 					nextGauge = 3;
+					canCreate = true;
+					break;
 				}
 			}
 
@@ -232,8 +233,7 @@ public class DecideDishes extends DecideRoles{
 
 			if(dishNumber == 9) { //味噌汁
 				if(FlavorX % 7163 != 0 || FlavorX % 7657 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "味噌") {
@@ -251,12 +251,13 @@ public class DecideDishes extends DecideRoles{
 					}
 					System.out.println("味噌汁を作って勝負します");
 					nextGauge = 4;
+					canCreate = true;
+					break;
 				}
 			}
 			if(dishNumber == 10) { //鍋
 				if(FlavorX % 12673 != 0 || FlavorX % 13547 != 0 || FlavorX % 16169 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "出汁") {
@@ -274,12 +275,13 @@ public class DecideDishes extends DecideRoles{
 					}
 					System.out.println("鍋を作って勝負します");
 					nextGauge = 4;
+					canCreate = true;
+					break;
 				}
 			}
 			if(dishNumber == 11) { //味噌煮込み豆腐
 				if(FlavorX % 9139 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "味噌") {
@@ -297,6 +299,8 @@ public class DecideDishes extends DecideRoles{
 					}
 					System.out.println("味噌煮込み豆腐を作って勝負します");
 					nextGauge = 4;
+					canCreate = true;
+					break;
 				}
 			}
 
@@ -305,8 +309,7 @@ public class DecideDishes extends DecideRoles{
 			if(dishNumber == 12) { //大盛り鍋
 				if(FlavorX % 207727 != 0 || FlavorX % 222053 != 0 || FlavorX % 265031 != 0
 						||FlavorX % 237367 != 0 || FlavorX % 283309 != 0 || FlavorX % 338143 != 0) {
-					System.out.println("この料理は作れません");
-					DecideDishes.selectDishes(); //やり直し
+					System.out.println("この料理は作れません"); //やり直し
 				}else {
 					for (int i = 0; i < numberOfCards; i++) {
 						if(myFlavors[i] == "出汁") {
@@ -324,6 +327,8 @@ public class DecideDishes extends DecideRoles{
 					}
 					System.out.println("大盛り鍋奴を作って勝負します");
 					nextGauge = 5;
+					canCreate = true;
+					break;
 				}
 			}
 
@@ -332,6 +337,8 @@ public class DecideDishes extends DecideRoles{
 			if(dishNumber == 100) {
 				System.out.println("料理を作らず勝負します");
 				nextGauge = 1;
+				canCreate = true;
+				break;
 			}
 
 		}	
--- src/P2/GameResult.java
+++ src/P2/GameResult.java
@@ -1,44 +1,47 @@
 package P2;
 
 public class GameResult extends DecideDishes{
-	
+
 	static int hungryDamage = 0;
 	static boolean a;
-	
+
 	public static void attackDamage() {
-		
+
 		a = false;
-		
+
 		if(a == false) { //自分か相手の手番を判断して、変えるゲージを決める
 			CPUNextGauge = nextGauge;
 			hungryDamage = roleNumber*CPUNextGauge;
+			for(int i = CPUCurrentGauge; i < hungryDamage; i++) {
+				CPUHungryGauge[i] = "■";
+			}
 			a = true;
 		}else {
 			myNextGauge = nextGauge; 
 			hungryDamage = roleNumber*myNextGauge;
+			for(int i = myCurrentGauge; i < hungryDamage; i++) {
+				myHungryGauge[i] = "■";
+			}
 			a = false;
 		}
-		
+
 		if(myNextGauge > 50) {
 			myNextGauge = 50;
 		}
 		if(CPUNextGauge > 50) {
 			CPUNextGauge = 50;
 		}
-		
-		for(int i = myCurrentGauge; i < myNextGauge; i++) {
-			myHungryGauge[i] = "■";
-		}
+
 		myCurrentGauge = myNextGauge;
+		CPUCurrentGauge = CPUNextGauge;
+		
 		System.out.print("自分の満腹ゲージ：");
 		for(int i = 0; i < 50; i++) {
 			System.out.print(myHungryGauge[i]);
 		}
 		System.out.print("\n");
-		
-		for(int i = CPUCurrentGauge; i < CPUNextGauge; i++) {
-			CPUHungryGauge[i] = "■";
-		}
+
+
 		CPUCurrentGauge = CPUNextGauge;
 		System.out.print("CPUの満腹ゲージ：");
 		for(int i = 0; i < 50; i++) {
@@ -46,16 +49,16 @@ public class GameResult extends DecideDishes{
 		}
 		System.out.print("\n");
 	}
-	
+
 	public static void finalResult() {
-		
+
 		if(CPUHungryGauge[49] == "■") {
 			System.out.print("お残し！あんたの勝ち！");
 		}
 		if(myHungryGauge[49] == "■") {
 			System.out.print("お残し！あんたの負け！");
 		}
-		
+
 	}
-	
+
 }
